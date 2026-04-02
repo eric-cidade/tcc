f8500f00b80bce26d74fdf52cceb34ce4c0e1b2d
Eric <ericmathias2012@gmail.com>
2026-04-02 15:28:56 -0300

    comentarios para arrumar dps

--- embedding.py
+++ embedding.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 # --- 1. Configurações Iniciais ---
 # Usando o modelo multilingue recomendado para português
 print("Carregando modelo de embedding...")
-model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2') # pesquisar se esse é o modelo adequado
 print("Modelo carregado com sucesso!")
 # Inicialização dos Clientes
 print("Conectando ao Meilisearch e ChromaDB...")
@@ -24,13 +24,12 @@ print("Conexões meili estabelecidas com sucesso!")
 chroma_client = chromadb.PersistentClient(path="./chroma_db")
 chroma_coll = chroma_client.get_or_create_collection(name="corpop_saude_ht")
 print("Conexões chroma estabelecidas com sucesso!")
-# Caminhos das pastas (ajuste se forem caminhos absolutos)
+
 PATH_ORIGINAL = './csv/ht/original'
 PATH_SIMPLIFICADA = './csv/ht/simplificada'
 PATH_CSV_MAP = './remedios_ht_map.csv'
 
 # --- 2. Carregar Mapeamento ---
-# Forçamos o ID como string para evitar problemas com zeros à esquerda
 df_map = pd.read_csv(PATH_CSV_MAP, dtype={'id': str})
 mapa_remedios = dict(zip(df_map['id'], df_map['nome']))
 print(f"Mapeamento carregado com {len(mapa_remedios)} medicamentos.")
@@ -39,7 +38,6 @@ def realizar_indexacao():
     print(f"Iniciando indexação de {len(mapa_remedios)} medicamentos...")
 
     for n_id, nome_remedio in mapa_remedios.items():
-        # Construção dos nomes de arquivo conforme seu padrão
         file_orig = f"{n_id}_original_limpo.txt"
         file_simp = f"{n_id}_validada_limpo.txt"
         
@@ -60,6 +58,8 @@ def realizar_indexacao():
                 embedding = model.encode(texto_hibrido).tolist()
 
                 # --- A. Indexar no ChromaDB (Busca Semântica/Sentido) ---
+                # no momento esta truncando o texto devido a um limite de tokens do modelo
+                # isso deve ser ajustado utilizando chunking ou outro modelo de embedding que aceite mais tokens
                 chroma_coll.add(
                     embeddings=[embedding],
                     documents=[texto_simp], # O que será retornado na busca semântica
